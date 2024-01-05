a084bb4a1c860a9277deb2fb1a4b3f746e7d6c22
kikokozz <dmiroshnikov_00@mail.ru>
2024-01-05 22:58:03 +0600

    1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,9 +2,11 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
         int howMany;
         System.out.println("На сколько человек необходимо разделить счёт?");
+
         while (true) {
             if (!scanner.hasNextInt()) {
                 System.out.println("Простите, возникла ошибка, не могли бы вы ввести число. На скольких человек необходимо разделить счёт?");
@@ -20,9 +22,28 @@ public class Main {
 
         }
 
+        Calculator calculator = new Calculator();
 
-
-
-
+        while (true) {
+            scanner.nextLine();
+            System.out.println("Пожалуйста, введите название товара");
+            String tovar = scanner.nextLine();
+            System.out.println("Пожалуйста, введите стоимость товара в формате \"рубли,копейки\"");
+            if (scanner.hasNextDouble()) {
+                double cost = scanner.nextDouble();
+                calculator.tovarName(tovar, cost);
+                System.out.println("Если желаете заказать что-то ещё, напишите любой символ \n" + "Если желаете закончить заказ, напишите \"Завершить\"");
+            }
+            else {
+                System.out.println("Введено некорректное значение цены товара, пожалуйста, повторите ввод");
+            }
+            String nextVvod = scanner.next();
+            if (nextVvod.equalsIgnoreCase("Завершить")) {
+                calculator.check(howMany);
+                System.out.println(calculator.price());
+                break;
+            }
+        }
+        scanner.close();
     }
 }
